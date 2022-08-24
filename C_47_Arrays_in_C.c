528f78ab53dbbeb15638356e457115c831d6f635
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-08-25 00:17:55 +0500

    Array in C Programming takes input from user

--- C_47_Arrays_in_C.c
+++ C_47_Arrays_in_C.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
+  int a;
+  scanf("%d", &a);
+  printf("%d", a);
   int arr[5] = {0, -1, 3, 5, 2};
   int arr_1[] = {1,2,3,4,5,6,7,8,9};
   int arr_2[5] = {1,6,7};
@@ -18,6 +21,15 @@ int main()
   for(int i = 0;i<5;i++)
   {
     printf("%d\n", arr_2[i]);
+  }
+  int userInput[5];
+  for(int x = 0; x<5 ; x++)
+  {
+    scanf("%d", &userInput[x]);
+  }
+  for(int i = 0;i<5;i++)
+  {
+    printf("%d", userInput[i]);
   }
   return 0;
 }
